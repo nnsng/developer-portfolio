d431bf25c56a5660b1d376270f6d59e49eb93fd3
nnsng <ngocsang1201@gmail.com>
2024-08-13 23:06:25 +0700

    move information to .env

--- src/utils/data/contact.ts
+++ src/utils/data/contact.ts
@@ -1,12 +1,13 @@
 import type { Contact } from '@/types'
+import { env } from '../env'
 
 export const contactData: Contact = {
-  email: 'ngocsang1201@gmail.com',
-  phone: '(+84) 338 980 207',
-  address: 'Thu Duc, Ho Chi Minh, Vietnam',
-  github: 'https://github.com/nnsng',
+  email: env('NEXT_PUBLIC_EMAIL'),
+  phone: env('NEXT_PUBLIC_PHONE'),
+  address: env('NEXT_PUBLIC_ADDRESS'),
+  github: `https://github.com/${env('NEXT_PUBLIC_GITHUB_USERNAME')}`,
   // facebook: 'https://www.facebook.com/',
-  linkedIn: 'https://www.linkedin.com/in/sangngocnguyen',
+  linkedIn: `https://www.linkedin.com/in/${env('NEXT_PUBLIC_LINKEDIN_USERNAME')}`,
   // stackOverflow: 'https://stackoverflow.com/',
-  devUsername: 'nguyenngocsang',
+  devUsername: env('NEXT_PUBLIC_DEV_USERNAME'),
 }
--- src/utils/data/personal.ts
+++ src/utils/data/personal.ts
@@ -1,6 +1,6 @@
 import type { Personal } from '@/types'
-import { contactData } from './contact'
 import { env } from '../env'
+import { contactData } from './contact'
 
 const NAME = 'Nguyen Ngoc Sang'
 
@@ -11,6 +11,6 @@ export const personalData: Personal = {
   profile: '',
   designation: 'Front-end Developer',
   description: `My name is ${NAME}. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.`,
-  leetcode: 'https://leetcode.com/nguyenngocsang',
+  leetcode: `https://leetcode.com/${env('NEXT_PUBLIC_LEETCODE_ID')}`,
   resume: env('NEXT_PUBLIC_RESUME_URL'),
 }
